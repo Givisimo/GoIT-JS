a140cb25984da6c3c2295d70360ba4d9fa92f6bb
Denys Plietnikov <givisimo@gmail.com>
2019-04-06 04:56:03 +0300

    Correction.Again.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@
 
 let message;
 const ADMIN_PASSWORD = 'm4ng0h4ckz';
-let userInput;
-
-userInput = prompt('Введите пароль');
+let userInput = prompt('Введите пароль');
 
 if (userInput === null) {
   message = 'Отменено пользователем!';
@@ -23,30 +21,28 @@ const credits = 23580;
 const pricePerDroid = 3000;
 let quantity = prompt('Количество купленных дроидов');
 let totalPrice;
+let messageTask2;
 
-if (quantity !== null) {
+if (quantity === null) {
+  messageTask2 = 'Отменено пользователем!';
+} else {
   totalPrice = pricePerDroid * quantity;
 }
 
 if (totalPrice > credits) {
-  message = 'Недостаточно средств на счету!';
+  messageTask2 = 'Недостаточно средств на счету!';
 } else if (totalPrice < credits) {
-  message = `Вы купили ${quantity} дроидов, на счету осталось ${credits -
+  messageTask2 = `Вы купили ${quantity} дроидов, на счету осталось ${credits -
     totalPrice} кредитов`;
-} else {
-  message = 'Отменено пользователем!';
 }
-console.log(message);
+console.log(messageTask2);
 
 // ================#3==================
 
 let countryInput = prompt('Страна доставки');
-let priceChina = 100;
-let priceSthAm = 250;
-let priceAustralia = 170;
-let priceIndia = 80;
-let priceJamaica = 120;
+let price;
 let country;
+let messageTask3;
 
 if (countryInput !== null) {
   country = countryInput.toLowerCase();
@@ -54,31 +50,40 @@ if (countryInput !== null) {
 
 switch (country) {
   case 'китай':
-    message = `Доставка в Китай будет стоить ${priceChina} кредитов`;
+    country = 'Китай';
+    price = 100;
     break;
 
   case 'южная америка':
-    message = `Доставка в Южную Америку будет стоить ${priceSthAm} кредитов`;
+    country = 'Южную Америку';
+    price = 250;
     break;
 
   case 'австралия':
-    message = `Доставка в Австралию будет стоить ${priceAustralia} кредитов`;
+    country = 'Австралию';
+    price = 170;
     break;
 
   case 'индия':
-    message = `Доставка в Индию будет стоить ${priceIndia} кредитов`;
+    country = 'Индию';
+    price = 80;
     break;
 
   case 'ямайка':
-    message = `Доставка в Ямайку будет стоить ${priceJamaica} кредитов`;
+    country = 'Ямайку';
+    price = 120;
     break;
 
   default:
-    message = 'В вашей стране доставка не доступна';
+    messageTask3 = 'В вашей стране доставка не доступна';
 }
 
 if (countryInput === null) {
-  message = 'Отменено пользователем';
+  messageTask3 = 'Отменено пользователем';
+  console.log(messageTask3);
+} else if (messageTask3 === 'В вашей стране доставка не доступна') {
+  console.log(messageTask3);
+} else {
+  messageTask3 = `Доставка в ${country} будет стоить ${price} кредитов`;
+  console.log(messageTask3);
 }
-
-console.log(message);
